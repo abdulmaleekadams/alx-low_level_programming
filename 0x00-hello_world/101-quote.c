fc81e9c7f29a1f6d28bca5e1e205c126afffc3ca
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-16 15:24:31 -0700

    Update code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 1 (error occurred)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-write(STDERR_FILENO,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof(message) - 1);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
