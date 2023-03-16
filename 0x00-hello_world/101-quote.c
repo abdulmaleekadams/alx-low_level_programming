c40e43b046317b195e08bc04c1299561b6f0bb89
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-16 15:10:24 -0700

    Update task 8 coding style to betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (error occurred)
+ */
 int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, sizeof(message) - 1);
-    return 1;
+        const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+        write(STDERR_FILENO, message, sizeof(message) - 1);
+        return (1);
 }
