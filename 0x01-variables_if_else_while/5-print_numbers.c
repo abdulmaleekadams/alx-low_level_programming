5de407c83e216155b3db4d54008fc636d471bbb3
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-17 17:11:00 -0700

    Add task 5 file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print all single digit number of base 10
+ * Return: 0 is return to the OS if program is successful
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+	printf("%d", i);
+	i++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
