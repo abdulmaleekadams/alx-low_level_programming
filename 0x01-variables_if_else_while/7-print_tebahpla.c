060ae6ef1e4c0ff2b36960f5b9e99043f908e250
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-17 17:24:35 -0700

    Ask task 7 file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - a program that display all lower case of alphabet
+ * Return: 0 is returned to the OS if the program is successful
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+	putchar(alpha);
+	alpha++;
+	}
+
+	putchar('\n');
+	return (0);
+}
