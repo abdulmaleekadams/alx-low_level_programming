53d5b310be93955cf0e8b420de9216b7ab4494bf
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-22 00:21:12 +0100

    First Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - a function that print "_putchar" followed by a new line
+ *
+ * Return: 0 if the function execute successsfully
+ */
+
+int main(void)
+{
+	char message[8] = "_putchar";
+	
+	int message_length = strlen(message);
+
+	int i = 0;
+	
+	while (i < message_length)
+	{
+		_putchar(message[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
