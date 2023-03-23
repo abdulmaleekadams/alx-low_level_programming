c94d7640f251f0fdc2f3ef1fcd135c5ebf3ebe4f
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-23 14:17:01 +0100

    Final update on task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,28 +16,25 @@ void times_table(void)
 		{
 			int product = row * column;
 
-			if (product >= 10)
-			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
-			}
+			if ( column== 0)
+				_putchar(product + '0');
+			
 			else
 			{
-
-				_putchar(product + '0');
-			}
-
-			/* The separator (comma and space) if not on last column */
-			if (column != 9)
-			{
-				_putchar(',');
-
-				if (product >= 10)
+				if (product < 10)
+				{
+					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+
 				else
 				{
+					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
 				}
 			}
 		}
