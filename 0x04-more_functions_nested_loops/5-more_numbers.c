afc7966a2b00a6b0157747eaf7ca5ca52840c1d2
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-24 15:16:45 +0100

    Add task 5 file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - display numbers from 0 - 14 to 10 times
+ *
+ * Return: the functions returns nothing
+ */
+
+void more_numbers(void)
+{
+	int counter, num;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 10)
+				_putchar('1');
+			_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
