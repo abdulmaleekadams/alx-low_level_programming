cc140f1dd8cfe27b7b6a3cb7d51b7c5ee4b1528b
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-24 12:35:00 +0100

    Add task 2 file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - a function that multiplies two integers
+ *
+ * @a: first integer parameter to be multiplied
+ * @b: second integer parameter to be multiplied
+ *
+ * Return: the product of the two number
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
