1899e0b429ab1dd2e96cf8ddedcfc983c27ddca4
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-03-27 13:06:57 +0100

    Satisfy betty style doc

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string in reverse, followed by a new line
+ * print_rev - a func that prints a string in reverse, followed by a new line
  *
  * @s: string parameter to be printed
  *
