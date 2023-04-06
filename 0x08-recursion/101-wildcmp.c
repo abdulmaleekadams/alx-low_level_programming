a86208e41f3e117b7390f7b85178aa4c55dc6b71
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-04-06 04:03:22 +0100

    Update task 8 logic and sleep

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,13 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	{
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+	}
 
 	return (0);
 }
