cfb7e040b4572833173df6f8d71dc17d5ef2d77a
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-04-09 10:40:22 +0100

    Add task 3 file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a func that multiplies two numbers
+ *
+ * @argc: counts of command line arguments
+ * @argv: the program command line arguments
+ *
+ * Return: 0 if successful, 1 if there is an error
+ */
+
+int main(int argc, char *argv[])
+{
+	int product, num1, num2;
+
+	if ( argc != 3)
+	{
+		printf("Error\n");
+		
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 * num2;
+
+	printf("%d\n", product);
+
+	return (0);
+}
