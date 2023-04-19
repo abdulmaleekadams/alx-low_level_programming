8da8328cd0ee63d25c9f56ac3f915b1fd06e6bab
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-04-19 02:51:25 +0100

    This should get it fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,48 +1,97 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <string.h>
 /**
- * new_dog - creates a new dog structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strlen - a function to find the length of a string
+ * @s: the string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	return (a);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: the pointer to the buffer string copy
+ * @src: the string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, b;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (b = 0; b < len; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[b] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - a function that creates a new dog.
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
  *
- * Return: pointer to the new struct on success, otherwise NULL
+ * Return: pointer to the new struct dog (success), NULL if fail.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *ndog;
 
-	size_t name_len, owner_len;
+	int name_len, owner_len;
 
-	if (name == NULL || owner == NULL)
-		return NULL;
+	name_len = _strlen(name);
 
-	name_len = strlen(name);
-	owner_len = strlen(owner);
+	owner_len = _strlen(owner);
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	ndog = malloc(sizeof(dog_t));
+
+	if (ndog == NULL)
+
 		return (NULL);
 
-	dog->name = malloc(sizeof(name_len + 1));
-	if (dog->name == NULL)
+	ndog->name = malloc(sizeof(char) * (name_len + 1));
+
+	if (ndog->name == NULL)
 	{
-		free(dog);
+		free(ndog);
+
 		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(owner_len + 1));
-	if (dog->owner == NULL)
+	ndog->owner = malloc(sizeof(char) * (owner_len + 1));
+
+	if (ndog->owner == NULL)
 	{
-		free(dog->name);
-		free(dog);
+		free(ndog);
+
+		free(ndog->name);
+
 		return (NULL);
 	}
 
-	strcpy(dog->name, name);
-	strcpy(dog->owner, owner);
-	dog->age = age;
+	strcpy(ndog->name, name);
 
-	return (dog);
+	_strcpy(ndog->owner, owner);
+
+	ndog->age = age;
+	return (ndog);
 }
