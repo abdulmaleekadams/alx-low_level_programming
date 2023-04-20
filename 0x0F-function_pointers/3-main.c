b3c99842c0d8edbd73c0007da742231a067745d5
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-04-20 17:07:50 +0100

    Use given exit status

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,7 +7,7 @@
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 on success, otherwise exit(-1)
+ * Return: 0 on success, otherwise exit()
  */
 int main(int argc, char *argv[])
 {
@@ -19,19 +19,19 @@ int main(int argc, char *argv[])
 		num_1 = atoi(argv[1]);
 		num_2 = atoi(argv[3]);
 		op = argv[2];
-		if (((*op == '%' || *op == '/') && num_2 == 0) || op[1] != '\0')
+		if ((*op == '%' || *op == '/') && num_2 == 0)
 		{
 			printf("Error\n");
-			exit(-1);
+			exit(100);
 		}
-		if (get_op_func(op) == NULL)
+		if (get_op_func(op) == NULL || op[1] != '\0')
 		{
 			printf("Error\n");
-			exit(-1);
+			exit(100);
 		}
 		printf("%d\n", get_op_func(op)(num_1, num_2));
 		return (0);
 	}
 	printf("Error\n");
-	exit(-1);
+	exit(98);
 }
